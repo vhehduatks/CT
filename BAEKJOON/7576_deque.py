eb002eb4c0596b7bb8bca94d560d0fede2c3b3e6
vhehduatks <hegyna11@gmail.com>
2021-11-28 04:06:58 +0900

    그래프는 depue 쓰는게 빠르다 성공

---
+++ BAEKJOON/7576_deque.py
@@ -0,0 +1,42 @@
+import sys
+from collections import deque
+read=sys.stdin.readline
+
+m,n=map(int,read().split())
+Q=deque()
+
+def bfs():
+	while Q:
+		qi,qj=Q.popleft()
+
+		for i in range(4):
+			ni=qi+di[i]
+			nj=qj+dj[i]
+
+			if 0<=ni and ni<n and 0<=nj and nj<m:
+				if tmt[ni][nj]==0:
+					tmt[ni][nj]=tmt[qi][qj]+1
+					Q.append((ni,nj))
+
+tmt=[]
+for _ in range(n):
+	tmt.append(list(map(int,read().split())))	
+
+di=[0,0,1,-1]
+dj=[1,-1,0,0]
+for i in range(n):
+	for j in range(m):
+		if tmt[i][j]==1:
+			Q.append((i,j))
+
+bfs()
+def dayfunc():	
+	day=0
+	for i in range(n):
+		for j in range(m):
+			if tmt[i][j]==0:
+				return -1					
+			day=max(day,tmt[i][j])
+	return day-1
+
+print(dayfunc())
