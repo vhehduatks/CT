e2f9399b6fcffe5fd178b6eac67f457e9949a53a
vhehduatks <64114699+vhehduatks@users.noreply.github.com>
2022-02-08 00:23:29 +0900

    뱀게임 단순구현

--- BAEKJOON/3190.py
+++ BAEKJOON/3190.py
@@ -3,55 +3,114 @@ read=sys.stdin.readline
 
 N=int(read())
 K=int(read())
-graph=[[0]*N for _ in range(N)]
+graph=[[False]*(N+1) for _ in range(N+1)]
 
 for _ in range(K):
 	i,j=map(int,read().split())
-	graph[i][j]=1
+	graph[i][j]=True
 
 L=int(read())
-command=[]
-for _ in range(L):
-	command.append((read().strip().split()))
+snake=[(1,1)]
+pres_d=1
+cnt=0
+direction={'up':0,'right':1,'down':2,'left':3}
 
-score=cnt=i=j=direction=0
-
-di=[0,1,0,-1]
-dj=[1,0,-1,0]
-class Snake:
-	def __init__(self) -> None:
-		self.head_i=0
-		self.head_j=0
-		self.score=0
-		self.body=[]
-	def _move(self):
-		
-
-snake=Snake()
-
-
-otime,order=command.pop(0)
-while score<K:
-	
-	if cnt==int(otime):
-		if order=='D':
-			direction+=1
-		else:
-			direction-=1
-		if command:
-			otime,order=command.pop(0)
-	i=i+di[direction]
-	j=j+dj[direction]
-
-	if i<0 or i>N or j<0 or j>N :
-		cnt+=1
-		break
-
+def is_apple(i,j):
+	global graph
 	if graph[i][j]:
-		score+=1
-		graph[i][j]=0
+		graph[i][j]=False
+		return True
+	return False
 
-	cnt+=1
+def is_collapse(i,j):
+	if i<=0 or N<i or j<=0 or N<j:
+		return True
+	if (i,j) in snake:
+		return True
+	return False
+
+def move(snake,pres_d,sec):
+	global cnt
+	while sec>cnt:
+		cnt+=1
+		if pres_d==direction['up']:
+			pres_head_i,pres_head_j=snake[-1]
+			
+			next_head_i,next_head_j=pres_head_i-1,pres_head_j
+			if is_collapse(next_head_i,next_head_j):
+				return cnt
+			if is_apple(next_head_i,next_head_j):
+				
+				snake.append((next_head_i,next_head_j))
+				continue
+			snake.append((next_head_i,next_head_j))
+			snake.pop(0)
+			
+		elif pres_d==direction['right']:
+			pres_head_i,pres_head_j=snake[-1]
+			
+			next_head_i,next_head_j=pres_head_i,pres_head_j+1
+			if is_collapse(next_head_i,next_head_j):
+				return cnt
+			if is_apple(next_head_i,next_head_j):
+				
+				snake.append((next_head_i,next_head_j))
+				continue
+			snake.append((next_head_i,next_head_j))
+			snake.pop(0)
+			
+		elif pres_d==direction['down']:
+			pres_head_i,pres_head_j=snake[-1]
+			
+			next_head_i,next_head_j=pres_head_i+1,pres_head_j
+			if is_collapse(next_head_i,next_head_j):
+				return cnt
+			if is_apple(next_head_i,next_head_j):
+				
+				snake.append((next_head_i,next_head_j))
+				continue
+			snake.append((next_head_i,next_head_j))
+			snake.pop(0)
+			
+		elif pres_d==direction['left']:
+			pres_head_i,pres_head_j=snake[-1]
+			
+			next_head_i,next_head_j=pres_head_i,pres_head_j-1
+			if is_collapse(next_head_i,next_head_j):
+				return cnt
+			if is_apple(next_head_i,next_head_j):
+				
+				snake.append((next_head_i,next_head_j))
+				continue
+			snake.append((next_head_i,next_head_j))
+			snake.pop(0)
+			
+		else:
+			print('never reach here')
+	return 0
+		
+order=[]
+
+for _ in range(L):
+	s,d=read().split()
+	s=int(s)
+	order.append((s,d))
 
 
-print(cnt)
+m=0
+while 1:
+	if order:
+		s,d=order.pop(0)
+	else:
+		s=sys.maxsize-1
+	m=move(snake,pres_d,s)
+	if m:
+		break
+	if d=='L':
+		pres_d-=1
+		pres_d%=4
+	elif d=='D':
+		pres_d+=1
+		pres_d%=4
+
+print(m if m else cnt)
