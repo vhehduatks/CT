4f3320af2f67da2ad3c70a90346893078f764442
vhehduatks <64114699+vhehduatks@users.noreply.github.com>
2022-02-22 23:06:04 +0900

    더럽게 푼 주차장 요금 계산

--- Programmers/2022 KAKAO BLIND_03.py
+++ Programmers/2022 KAKAO BLIND_03.py
@@ -3,15 +3,13 @@
 # 	for line in records:
 # 		dtiem,carnum,info=line[0],line[1],line[2]
 
-car_list=[0]*10000
 gra={}
 
-def intime_func(carnum,dtime):
-	intime=gra[carnum]
+def intime_func(carnum,intime,outtime):
 	intime=list(map(int,intime.split(':')))
-	dtime=list(map(int,dtime.split(':')))
-	ret=(dtime[0]-intime[0])*60
-	ret+=dtime[1]-intime[1]
+	outtime=list(map(int,outtime.split(':')))
+	ret=(outtime[0]-intime[0])*60
+	ret+=outtime[1]-intime[1]
 	return ret
 
 def money_func(fees,time_sum):
@@ -22,36 +20,51 @@ def money_func(fees,time_sum):
 	else:
 		ret=fees[1]
 		remain=time_sum-fees[0]
-		ret=ret+(remain//fees[2]+1)*fees[3]
+		ret=ret+(remain//fees[2] if remain%fees[2]==0 else remain//fees[2]+1)*fees[3]
 	return ret
 
 
-
+total_time={}
+in_out={}
 def solution(fees, records):
-	ret=[]
-	out_cars=[]
+	ret={}
 	for line in records:
 		dtime,carnum,info=line.split()
 		if info=='IN':
 			gra[carnum]=dtime
+			in_out.setdefault(carnum,0)
+			total_time.setdefault(carnum,0)
+			in_out[carnum]+=1
 		elif info=='OUT':
-			time_sum=intime_func(carnum,dtime)
-			total=money_func(fees,time_sum)
-			ret.append([carnum,total])
-			out_cars.append(carnum)
-	
-	for line in records:
+			temp_time_sum=intime_func(carnum,gra[carnum],dtime)
+			total_time[carnum]+=temp_time_sum
+			in_out[carnum]-=1
+
+	for carnum,time_sum in total_time.items():
+		total=money_func(fees,time_sum)
+		ret.setdefault(carnum,total)
+		# ret[carnum]+=total
+		
+	for line in reversed(records):
 		dtime,carnum,_=line.split()
-		if not carnum in out_cars:
-			time_sum=intime_func(carnum,'24:00')
+		if in_out[carnum]:
+			in_out[carnum]-=1
+			time_sum=intime_func(carnum,dtime,'23:59')
+			time_sum+=total_time[carnum]
 			total=money_func(fees,time_sum)
-			ret.append([carnum,total])
+			ret[carnum]=total
 	
-	ret=sorted(ret,key=lambda x:x[0])
+	ret_=[]
+	for carnum,money in ret.items():
+		ret_.append([carnum,money])
+	
+	result=[]
+	for r in sorted(ret_):
+		result.append(r[1])
 
-	return ret
+	return result
 
-fees_=[180,500,10,600]
+fees_=[180,5000,10,600]
 record_=["05:34 5961 IN", "06:00 0000 IN", "06:34 0000 OUT", "07:59 5961 OUT", "07:59 0148 IN", "18:59 0000 IN", "19:09 0148 OUT", "22:59 5961 IN", "23:00 5961 OUT"]
 
 print(solution(fees_,record_))
