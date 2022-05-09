40f66e7c92a9140ea3e814d96675313d16483932
vhehduatks <hegyna11@gmail.com>
2022-05-10 00:14:53 +0900

    Update 15988.py

--- BAEKJOON/15988.py
+++ BAEKJOON/15988.py
@@ -1,13 +1,13 @@
 import sys
 read=sys.stdin.readline
-
+mod=1000000009
 n=int(read())
 
-dp=[0]*100
+dp=[0]*1000001
 dp[1],dp[2],dp[3]=1,2,4
 
-for i in range(4,100):
-	dp[i]=dp[i-1]+dp[i-2]+dp[i-3]
+for i in range(4,1000001):
+	dp[i]=(dp[i-1]%mod+dp[i-2]%mod+dp[i-3]%mod)%mod
 
 for _ in range(n):
 	print(dp[int(read())])
